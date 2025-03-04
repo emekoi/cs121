181c2a384239c2b5694089a4e1f894029d360192
Emeka Nkurumeh <emekankurumeh@outlook.com>
2025-03-04 03:31:49 -0800

    rewrote sql setup and added stubs for sql files

--- app.py
+++ app.py
@@ -11,46 +11,39 @@ import pylast
 
 # GENERAL UTIL FUNCTIONS
 # ------------------------------------------------------------------------------
-def fatal(msg):
+def log(msg):
     print(msg, file=sys.stderr)
-    sys.exit(1)
 
 
 # MYSQL UTIL FUNCTIONS
 # ------------------------------------------------------------------------------
-def mysql_setup() -> mysql.connector.MySQLConnection:
-    """ "
-    Returns a connected MySQL connector instance, if connection is successful.
-    If unsuccessful, exits.
-    """
-    try:
-        return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            port="3306",
-            password="",
-            database="finalprojectdb",
-        )
-    except mysql.connector.Error as err:
-        # Remember that this is specific to _database_ users, not
-        # application users. So is probably irrelevant to a client in your
-        # simulated program. Their user information would be in a users table
-        # specific to your database; hence the DEBUG use.
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR and DEBUG:
-            sys.stderr("Incorrect username or password when connecting to DB.")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR and DEBUG:
-            sys.stderr("Database does not exist.")
-        elif DEBUG:
-            sys.stderr(err)
-        else:
-            # A fine catchall client-facing message.
-            sys.stderr("An error occurred, please contact the administrator.")
-        return None
+def mysql_init(database: str) -> mysql.connector.MySQLConnection:
+    # attempt to connect to MySQL
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        port="3306",
+        password="",
+    )
 
+    try:
+        # attempt to connect to the specified database
+        connection.databse = database
+    except mysql.connector.Error as err:
+        # if the database doesn't exist, attempt to create it and connect
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            with connection.cursor() as cursor:
+                cursor.execute("CREATE DATABASE %s", (database,))
+                connection.databse = database
+                # TODO: setup database here
+        else:
+            raise err
+
+    return connection
 
 # LASTFM UTIL FUNCTIONS
 # ------------------------------------------------------------------------------
-def lastfm_setup() -> pylast.LastFMNetwork:
+def lastfm_init() -> pylast.LastFMNetwork:
     API_KEY = os.getenv("API_KEY")
     API_SECRET = os.getenv("API_SECRET")
 
@@ -58,6 +51,7 @@ def lastfm_setup() -> pylast.LastFMNetwork:
         return None
 
     return pylast.LastFMNetwork(API_KEY, API_SECRET)
+
 
 def lastfm_user_create(
     network: pylast.LastFMNetwork, retry_count: int
@@ -77,25 +71,32 @@ def lastfm_user_create(
             time.sleep(1)
 
 
-def lastfm_user_auth(
-    network: pylast.LastFMNetwork, retry_count: int
-) -> tuple[str, str]:
-    pass
-
-
-# MAIN FUNCTION
+# MAIN FUNCTIONS
 # ------------------------------------------------------------------------------
-def main():
-    n = setup()
-    x = user_auth(n, 10)
-    print(f"{x}")
+def user_auth(username: str, password: str):
+    with mysql_connection.cursor() as cursor:
+        cursor.execute("SELECT @@version")
+        res = cur.fetchone()
+        print(res[0])
 
+def main():
+    # n = setup()
+    # x = user_auth(n, 10)
+    # print(f"{x}")
+    print(10)
 
 if __name__ == "__main__":
-    if (mysql_connection := mysql_setup()) is None:
+    try:
+        mysql_connection = mysql_init("finalprojectdb")
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            log("Incorrect username or password when connecting to DB")
+        else:
+            log(f"Failed to connect to DB: {err}")
+
         sys.exit(1)
 
-    lastfm_network = lastfm_setup()
+    lastfm_network = lastfm_init()
 
     main()
 
